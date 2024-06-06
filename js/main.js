fc160b7df56279d160041d5245e1145a91c16460
Flaw5252 <94564997+Flaw5252@users.noreply.github.com>
2024-06-06 12:20:55 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-console.warn(
-  "%cHello!!",
-  "color: red; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
-  "Feel free to use anything you find here for your projects; credit is appreciated but not required! Visit my website at https://3kh0.net for more information."
-);
-
 // this setting controls if ads are shown,
 // more info on the README.md file
 var adStatus = localStorage.getItem("adConsent") === 'true'; // default: true
